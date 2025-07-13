63bf8b35a4942d116afd71351f6dc09adf18071e
linuxbender <linuxbender@gmail.com>
2025-07-13 21:47:28 +0200

    Add initial Spring Boot application structure and configuration files

---
+++ kata-02/modules/shared/backend/src/test/java/ch/kata/kata_02/Kata02ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.kata.kata_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kata02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ kata-02/shell/backend/src/main/java/ch/kata/kata_02/Kata02Application.java
@@ -0,0 +1,13 @@
+package ch.kata.kata_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kata02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kata02Application.class, args);
+	}
+
+}
