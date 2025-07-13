8f4ed34fc3c99fdb8b41fa971ac8b84203280bae
linuxbender <linuxbender@gmail.com>
2025-07-13 23:33:04 +0200

    Rename Kata02 application and test classes to reflect their respective modules; update POM files to use backend suffix for artifact IDs

--- kata-02/modules/shared/backend/src/main/java/ch/kata/kata_02/SharedApplication.java
+++ kata-02/modules/shared/backend/src/main/java/ch/kata/kata_02/SharedApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Kata02Application {
+public class SharedApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Kata02Application.class, args);
+		SpringApplication.run(SharedApplication.class, args);
 	}
 
 }
