c4e252f426b43adb9c483f9316b9ca134f71fe3f
linuxbender <linuxbender@gmail.com>
2026-03-10 20:04:47 +0100

    feat: Initialize Spring Boot application with Maven wrapper and configuration files

---
+++ kata-04/src/main/java/ch/theforce/kata_04/Kata04Application.java
@@ -0,0 +1,13 @@
+package ch.theforce.kata_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kata04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kata04Application.class, args);
+	}
+
+}
---
+++ kata-04/src/test/java/ch/theforce/kata_04/Kata04ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.theforce.kata_04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kata04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
